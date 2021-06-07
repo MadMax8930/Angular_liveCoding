64fcb0be4292d82130b3c64d659900846bc76113
Jérémy Bojko <jeremy.bojko@prontonmail.com>
2021-06-07 14:18:21 +0200

    Add comment

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -19,6 +19,7 @@ export class AuthService {
       map(
         (resp: any) => {
           localStorage.setItem('TOKEN_APPLI', resp.token);
+          console.log('Token Save');
           return resp;
         }
       )
